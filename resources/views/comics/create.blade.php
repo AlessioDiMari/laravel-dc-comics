81268af338caea227bf279e258f7533984a114ce
AlessioDiMari <alessiodimari.adm25@gmail.com>
2024-04-17 00:29:41 +0200

    creato il create

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -4,44 +4,44 @@
     
     <div class="container py-5">
 
-        <form action="">
+        <form action="{{route('comics.store')}}" method="POST">
 
             @csrf
             <div class="mb-3">
                 <label for="title" class="form-label">Titolo</label>
-                <input type="email" class="form-control" id="title" aria-describedby="emailHelp">
+                <input type="text" class="form-control" name="title" id="title" aria-describedby="emailHelp" required>
             </div>
             <div class="mb-3">
                 <label for="description" class="form-label">Descrizione</label>
-                <input type="email" class="form-control" id="description" aria-describedby="emailHelp">
+                <textarea type="text" class="form-control" name="description" id="description" aria-describedby="emailHelp" required></textarea>
             </div>
             <div class="mb-3">
                 <label for="thumb" class="form-label">Immagine</label>
-                <input type="email" class="form-control" id="thumb" aria-describedby="emailHelp">
+                <input type="text" class="form-control" name="thumb" id="thumb" aria-describedby="emailHelp" required>
             </div>
             <div class="mb-3">
                 <label for="price" class="form-label">Prezzo</label>
-                <input type="email" class="form-control" id="price" aria-describedby="emailHelp">
+                <input type="text" class="form-control" name="price" id="price" aria-describedby="emailHelp" required>
             </div>
             <div class="mb-3">
                 <label for="series" class="form-label">Serie</label>
-                <input type="email" class="form-control" id="series" aria-describedby="emailHelp">
+                <input type="text" class="form-control" name="series" id="series" aria-describedby="emailHelp" required>
             </div>
             <div class="mb-3">
                 <label for="sale_date" class="form-label">Data pubblicazione</label>
-                <input type="email" class="form-control" id="sale_date" aria-describedby="emailHelp">
+                <input type="date" class="form-control" name="sale_date" id="sale_date" aria-describedby="emailHelp" required>
             </div>
             <div class="mb-3">
                 <label for="type" class="form-label">Tipo</label>
-                <input type="email" class="form-control" id="type" aria-describedby="emailHelp">
+                <input type="text" class="form-control" name="type" id="type" aria-describedby="emailHelp" required>
             </div>
             <div class="mb-3">
                 <label for="artists" class="form-label">Artisti</label>
-                <input type="email" class="form-control" id="artists" aria-describedby="emailHelp">
+                <input type="text" class="form-control" name="artists" id="artists" aria-describedby="emailHelp">
             </div>
             <div class="mb-3">
                 <label for="writers" class="form-label">Scrittori</label>
-                <input type="email" class="form-control" id="writers" aria-describedby="emailHelp">
+                <input type="text" class="form-control" name="writers" id="writers" aria-describedby="emailHelp">
             </div>
 
             <button type="submit" class="btn btn-primary">Aggiungi</button>
